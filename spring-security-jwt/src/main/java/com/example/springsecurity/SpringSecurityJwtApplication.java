342c60318021e5bb118930308d18356a6c8abd59
HiiWee <wpdlzhf159@gmail.com>
2023-06-30 18:44:07 +0900

    rename: main 메서드 모듈 이름에 맞게 변경

--- spring-security-jwt/src/main/java/com/example/springsecurity/SpringSecurityJwtApplication.java
+++ spring-security-jwt/src/main/java/com/example/springsecurity/SpringSecurityJwtApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurityApplication {
+public class SpringSecurityJwtApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringSecurityApplication.class, args);
+        SpringApplication.run(SpringSecurityJwtApplication.class, args);
     }
 
 }
