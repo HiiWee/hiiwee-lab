e6a682ae65f8cdfe7fb9a2f167662056da6fe8c2
HiiWee <wpdlzhf159@gmail.com>
2023-08-07 23:43:12 +0900

    feat: security config 주석 추가

--- spring-security-jwt/src/main/java/com/example/springsecurity/config/SecurityConfig.java
+++ spring-security-jwt/src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -41,27 +41,35 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // CSRF 설정 Disable
+
         http.csrf().disable()
-                .cors().and()
+                .formLogin().disable()
+                .cors()
+                .and()
+
+                // 인증/인가에 대한 예외 처리
                 .exceptionHandling()
                 // 인증 실패시 예외 처리(사용자가 인증되지 않음)
                 .authenticationEntryPoint(new RestAuthenticationEntryPoint(mapper))
                 // 인가 실패시 예외 처리 (허락되지 않은 접근)
                 .accessDeniedHandler(new RestAccessDeniedHandler(mapper))
                 .and()
-                // JWT를 사용하므로 세션 사용하지 않음
+
+                // JWT를 사용하므로 세션을 사용하지 않음
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 // 로그인, 회원가입 API는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
                 .and()
+
                 .authorizeRequests()
                 .antMatchers("/signin").permitAll()
                 .antMatchers("/members/signup").permitAll()
+                .antMatchers("/hello").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
-                .apply(new JwtSecurityConfig(jwtTokenProvider, mapper))
-                .and()
-                .formLogin().disable();
+
+                .apply(new JwtSecurityConfig(jwtTokenProvider, mapper));
+
         return http.build();
     }
 }
