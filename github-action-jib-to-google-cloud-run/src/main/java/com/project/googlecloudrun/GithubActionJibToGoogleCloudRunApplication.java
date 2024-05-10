c7ce69c3c3ac739a76c99e69c7c3aa8b706f49bc
HiiWee <wpdlzhf159@gmail.com>
2024-05-11 03:35:12 +0900

    init: project

---
+++ github-action-jib-to-google-cloud-run/src/main/java/com/project/googlecloudrun/GithubActionJibToGoogleCloudRunApplication.java
@@ -0,0 +1,13 @@
+package com.project.googlecloudrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionJibToGoogleCloudRunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionJibToGoogleCloudRunApplication.class, args);
+    }
+
+}
---
+++ github-action-jib-to-google-cloud-run/src/test/java/com/project/googlecloudrun/GithubActionJibToGoogleCloudRunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.googlecloudrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionJibToGoogleCloudRunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
